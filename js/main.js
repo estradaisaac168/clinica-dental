62537311b9979dec8c576280042d911e0d1cb1ea
estradaisaac168 <estradaisaac168@gmail.com>
2025-01-28 06:02:15 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-// Add JS here
+document.addEventListener('DOMContentLoaded', () => {
+    const backToTopButton = document.getElementById('backToTop');
+    if (backToTopButton) {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 200) {
+                backToTopButton.classList.add('show');
+            } else {
+                backToTopButton.classList.remove('show');
+            }
+        });
+
+        backToTopButton.addEventListener('click', () => {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth',
+            });
+        });
+    }
+});
